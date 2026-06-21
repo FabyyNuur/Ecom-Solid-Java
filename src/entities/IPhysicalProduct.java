2e9947425133cd799fc685a3f27106415a3c3ff1
FabyyNuur <uicode.byfatoubintg@gmail.com>
2026-06-21 14:44:58 +0000

    ishippable

--- src/entities/IPhysicalProduct.java
+++ src/entities/IPhysicalProduct.java
@@ -1,9 +1,8 @@
 package entities;
 
 
-public interface IPhysicalProduct extends IProduct {
+public interface IPhysicalProduct extends IProduct, IShippable {
     void checkStock(int quantity) throws Exception;
     void deductStock(int quantity) throws Exception;
     String getStockDisplay();
-    float calculateShippingCost();
 }
--- src/services/PricingService.java
+++ src/services/PricingService.java
@@ -1,10 +1,25 @@
 package services;
 
 import discount.IDiscountStrategy;
+import entities.IShippable;
+import entities.IProduct;
 import entities.Order;
+import entities.OrderLine;
 
 public class PricingService {
     public float calculateTotal(Order order, IDiscountStrategy discountStrategy) {
-        return discountStrategy.applyDiscount(order.getSubtotal());
+        float discountedSubtotal = discountStrategy.applyDiscount(order.getSubtotal());
+        return discountedSubtotal + calculateShipping(order);
+    }
+
+    private float calculateShipping(Order order) {
+        float shipping = 0;
+        for (OrderLine line : order.getLines()) {
+            IProduct product = line.getProduct();
+            if (product instanceof IShippable shippable) {
+                shipping += shippable.calculateShippingCost() * line.getQuantity();
+            }
+        }
+        return shipping;
     }
 }
