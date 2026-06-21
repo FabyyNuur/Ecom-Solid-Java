d126fb03157c42b9a636c73309ccd6155847c948
FabyyNuur <uicode.byfatoubintg@gmail.com>
2026-06-21 09:35:15 +0000

    getter

--- src/entity/Product.java
+++ src/entity/Product.java
@@ -10,4 +10,33 @@ public class Product {
         this.price = price;
         this.stock = stock;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [name=" + name + ", price=" + price + ", stock=" + stock + "]";
+    }
 }
